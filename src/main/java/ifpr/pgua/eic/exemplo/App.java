2ca63d1a14a2dea28ed56578fa2c4fb548ecefc3
Heloisa Tibilletti <heloisa.tibilletti@gmail.com>
2023-06-27 12:03:10 -0300

    Código

--- src/main/java/ifpr/pgua/eic/exemplo/App.java
+++ src/main/java/ifpr/pgua/eic/exemplo/App.java
@@ -2,6 +2,11 @@ package ifpr.pgua.eic.exemplo;
 
 import java.util.ArrayList;
 
+import ifpr.pgua.eic.exemplo.controllers.TelaCadastro;
+import ifpr.pgua.eic.exemplo.controllers.TelaPrincipal;
+import ifpr.pgua.eic.exemplo.controllers.TelaVisualizar;
+import ifpr.pgua.eic.exemplo.infra.Escritor;
+import ifpr.pgua.eic.exemplo.infra.Leitor;
 import ifpr.pgua.eic.exemplo.models.Estacionamento;
 import io.github.hugoperlin.navigatorfx.BaseAppNavigator;
 import io.github.hugoperlin.navigatorfx.ScreenRegistryFXML;
@@ -27,7 +32,12 @@ public class App extends BaseAppNavigator{
     public void init() throws Exception {
         super.init();
 
+        Leitor leitor = new Leitor();
+
+        ArrayList<Veiculo> lista = leitor.carregar("veiculos");
+
         gerenciador = new Estacionamento("SuperEstac", "1234-1234");
+
     }
 
     /*método para indicar qual é a tela inicial da aplicação */
@@ -42,10 +52,30 @@ public class App extends BaseAppNavigator{
         return "Quiz App";
     }
 
+    @Override
+    public void stop() throws Exception {
+        // TODO Auto-generated method stub
+        super.stop();
+
+        Escritor escritor = new Escritor();
+        escritor.salvar("veiculos.txt",gerenciador.listaTodosVeiculos());
+    }
+
     /*método para registrar as telas da aplicação*/
     @Override
     public void registrarTelas() {
-        
-    }
+        registraTela("PRINCIPAL",
+                      new ScreenRegistryFXML(App.class, 
+                                         "principal.fxml", 
+                                          o->new TelaPrincipal()));
+        registraTela("CADASTRO", 
+                      new ScreenRegistryFXML(App.class, 
+                                             "cadastro.fxml",
+                                            o->new TelaCadastro(gerenciador)));
+        registraTela("VISUALIZAR",
+                     new ScreenRegistryFXML(App.class, 
+                                           "visualizar.fxml", o->new TelaVisualizar(gerenciador)));                                
+    
+        }
     
 }
---
+++ src/main/java/ifpr/pgua/eic/exemplo/Veiculo.java
@@ -0,0 +1,5 @@
+package ifpr.pgua.eic.exemplo;
+
+public class Veiculo {
+
+}
